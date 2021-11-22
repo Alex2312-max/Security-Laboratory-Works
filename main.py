8d29e5113d65f8dcaeee08e5ad9531c16afd3fcf
Alex Clefos <aclefos@yahoo.com>
2021-11-22 21:01:41 +0200

    'LaboratoryWork3'

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import os
 import sys
 import re
 import json
+import subprocess
 
 
 class MainWindow(QMainWindow):
@@ -18,11 +19,12 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
 
         layout = QVBoxLayout()
-        self.editor = QPlainTextEdit()  # Could also use a QTextEdit and set self.editor.setAcceptRichText(False)
+        self.editor = QPlainTextEdit()
         self.resize(600, 450)
         self.number_of_instances = 0
         self.tree_window = None
         self.information = 0
+        self.audit_result_window = None
 
         # Setup the QTextEdit editor configuration
         fixedfont = QFontDatabase.systemFont(QFontDatabase.FixedFont)
@@ -104,6 +106,11 @@ class MainWindow(QMainWindow):
         refresh_action.setStatusTip("Refresh button")
         search_toolbar.addAction(refresh_action)
 
+        audit_action = QAction("Audit Workstation", self)
+        audit_action.triggered.connect(self.show_audit_workstation_window)
+        audit_action.setStatusTip("Audit workstation by selected options.")
+        search_toolbar.addAction(audit_action)
+
         self.update_title()
         self.show()
 
@@ -121,36 +128,40 @@ class MainWindow(QMainWindow):
                 with open(path) as f:
                     text = f.readlines()
                 text = self.transform_to_json_format(str(text))
+                self.editor.setPlainText(json.dumps(self.information, indent=4, sort_keys=True))
             except Exception as e:
                 self.dialog_critical(str(e))
 
             else:
                 self.path = path
-                self.editor.setPlainText(json.dumps(text, indent=4, sort_keys=True))
+                self.editor.setPlainText(json.dumps(self.information, indent=4, sort_keys=True))
                 self.update_title()
 
     def transform_to_json_format(self, text):
 
         lines = re.sub('\n', '', text)
-        lines = re.findall('<custom_item>(.*?)<\/custom_item>', lines)
+        lines = re.findall('<custom_item>(.*?)</custom_item>', lines)
 
         li = []
         for element in lines:
 
             mapper = {}
-            element = re.sub('\n', '', element)
-            element = re.sub('\s+', ' ', element)
+            element = re.sub('\\\\n', '', element)
+            element = re.sub('\\\\\\\\', '\\\\', element)
 
+            element = re.sub('\'', '', element)
+            element = re.sub(' , ', ' ', element)
+            element = re.sub('\s+', ' ', element)
             element = element.strip()
-            element = re.findall('(\w+)\s:\s(\w+ |".*?")', element)
+            element = re.findall('(\w+)\s+:\s(\w+|\".*?\")', element)
 
             for key, value in element:
+                value = re.sub('\"', '', value)
                 mapper[key] = value
             li.append(mapper)
-
+        print(li)
         self.number_of_instances = len(li)
         self.information = li
-        print(self.number_of_instances)
         return li
 
     def file_save(self):
@@ -202,9 +213,17 @@ class MainWindow(QMainWindow):
             self.statusbar.showMessage("This is the last iteration founded")
         return
 
+    def show_audit_workstation_window(self):
+        print(len(self.information), self.information)
+        # try:
+        self.audit_result_window = AuditWindow(self.path, self.information)
+        print('good')
+        self.audit_result_window.show()
+        # except:
+             # print('No information uploaded')
+
     def show_new_window(self):
         try:
-
             self.tree_window = Window(self.path, self.number_of_instances, self.information)
             self.tree_window.show()
             if self.tree_window.block_count:
@@ -219,8 +238,153 @@ class MainWindow(QMainWindow):
             for element in self.tree_window.block_count:
                 new_information.append(self.information[element])
 
+            self.number_of_instances = len(new_information)
             self.information = new_information
             self.editor.setPlainText(json.dumps(self.information, indent=4, sort_keys=True))
+
+
+class AuditWindow(QWidget):
+    # def __init__(self, path, information):
+    #     super().__init__()
+    #     self.path = re.findall(r'[^\/]+(?=\.)', path)[0]
+    #     self.number_of_instances = len(self.information)
+    #
+    #     self.information = information
+    #     self.results = []
+    #     self.setWindowTitle(self.path)
+    #     self.resize(400, 100)
+    #     self.run_cmd()
+    #     # Create an outer layout
+    #     self.layout = QVBoxLayout()
+    #
+    #     optionsLayout = QVBoxLayout()
+    #
+    #     # groupBox.setLayout(optionsLayout)
+    #
+    #     for element, result in zip(range(self.number_of_instances), self.results):
+    #         label = QLabel(f"Block{element} - {result}")
+    #         optionsLayout.addWidget(label)
+    #
+    #     self.layout.addLayout(optionsLayout)
+    #     # Set the window's main layout
+    #     self.setLayout(self.layout)
+    #     self.show()
+    def __init__(self, path, information):
+        super().__init__()
+        self.path = re.findall(r'[^\/]+(?=\.)', path)[0]
+        self.number_of_instances = len(information)
+        self.information = information
+        self.setWindowTitle('Audit Workstation')
+
+        self.resize(400, 100)
+        # Create an outer layout
+        outerLayout = QVBoxLayout()
+        # Create a form layout for the label and line edit
+        topLayout = QFormLayout()
+        # self.block_count = []
+        self.policies_check = []
+
+        self.run_cmd()
+        # Add a label and a line edit to the form layout
+        # self.lineEdit = QLineEdit(self)
+        # self.lineEdit.setStatusTip('Search and mark the block that contain the syntax')
+        # topLayout.addRow(":", self.lineEdit)
+
+        # Create a layout for the checkboxes
+        optionsLayout = QVBoxLayout()
+        # Add some checkboxes to the layout
+        groupBox = QGroupBox("Content")
+
+        self.checkBoxes = []
+        for i, policy_status in zip(range(self.number_of_instances), self.policies_check):
+            if policy_status == 'Passed':
+                checkBox = QCheckBox(f"Block - {i} ____ Policy Status: {policy_status}")
+                checkBox.setStyleSheet('color : green')
+                optionsLayout.addWidget(checkBox)
+                self.checkBoxes.append(checkBox)
+            else:
+                checkBox = QCheckBox(f"Block - {i} ____ Policy Status: {policy_status}")
+                checkBox.setStyleSheet('color : red')
+                optionsLayout.addWidget(checkBox)
+                self.checkBoxes.append(checkBox)
+
+        self.checkBoxNone = QCheckBox("Select none")
+        self.checkBoxNone.setCheckState(Qt.Unchecked)
+        self.checkBoxNone.stateChanged.connect(
+            partial(self.selectBoxes, False))
+        optionsLayout.addWidget(self.checkBoxNone)
+
+        self.checkBoxAll = QCheckBox("Select All")
+        self.checkBoxAll.setCheckState(Qt.PartiallyChecked)
+        self.checkBoxAll.stateChanged.connect(
+            partial(self.selectBoxes, True))
+        optionsLayout.addWidget(self.checkBoxAll)
+
+        groupBox.setLayout(optionsLayout)
+        scroll = QScrollArea()
+        scroll.setWidget(groupBox)
+        scroll.setWidgetResizable(True)
+        scroll.setFixedHeight(400)
+        outerLayout.addWidget(scroll)
+
+        # Button to records the indexes that where chosen
+        btn = QPushButton("Apply", self)
+        btn.setToolTip("Apply changes")
+        btn.clicked.connect(self.run_cmd)
+        topLayout.addWidget(btn)
+
+        # btn_line = QPushButton("Check policies", self)
+        # btn_line.clicked.connect(self.run_cmd)
+        # topLayout.addWidget(btn_line)
+
+        outerLayout.addLayout(topLayout)
+        outerLayout.addLayout(optionsLayout)
+
+        # Set the window's main layout
+        self.setLayout(outerLayout)
+
+    def checkStates(self):
+        states = [c.isChecked() for c in self.checkBoxes]
+
+        # temporarily block signals so that there is no recursive calls
+        self.checkBoxAll.blockSignals(True)
+        self.checkBoxNone.blockSignals(True)
+
+        # set the "select all" fully checked too if all boxes are checked, otherwise make it partially checked
+        self.checkBoxAll.setCheckState(
+            Qt.Checked if all(states) else Qt.PartiallyChecked)
+
+        # set the "select none" unchecked only if all boxes are unchecked, otherwise make it partially checked
+        self.checkBoxNone.setCheckState(
+            Qt.Unchecked if not any(states) else Qt.PartiallyChecked)
+
+        # unblock signals back
+        self.checkBoxAll.blockSignals(False)
+        self.checkBoxNone.blockSignals(False)
+
+    def selectBoxes(self, state):
+
+        for check in self.checkBoxes:
+
+            check.blockSignals(True)
+            check.setChecked(state)
+            check.blockSignals(False)
+        self.checkStates()
+
+    def run_cmd(self):
+        for block in self.information:
+
+            try:
+                out = os.system(block['cmd'])
+                out_filtered = re.search('(\w+)|(\d+)', out)
+                expected_filter = re.search('(\w+)|(\d+)', block['expec'])
+                # if out == block['expect']:
+                if any(element in expected_filter for element in out_filtered):
+                    self.policies_check.append('Passed')
+                else:
+                    self.policies_check.append('Not passed')
+            except:
+                self.policies_check.append('Not passed')
 
 
 class Window(QWidget):
@@ -229,6 +393,7 @@ class Window(QWidget):
         self.path = re.findall(r'[^\/]+(?=\.)', path)[0]
         self.number_of_instances = number_of_instances
         self.information = information
+
         self.setWindowTitle(self.path)
         self.resize(400, 100)
         # Create an outer layout
@@ -272,15 +437,14 @@ class Window(QWidget):
         scroll.setWidgetResizable(True)
         scroll.setFixedHeight(400)
         outerLayout.addWidget(scroll)
+
         # Button to records the indexes that where chosen
-        btn = QPushButton("Apply and Close", self)
+        btn = QPushButton("Apply", self)
         btn.setToolTip("Close Application")
         btn.clicked.connect(self.checkStates_block_index)
         topLayout.addWidget(btn)
 
         btn_line = QPushButton("Search text", self)
-        # btn_line.setToolTip("Close Application")
-
         btn_line.clicked.connect(self.search_text_blocks)
 
         topLayout.addWidget(btn_line)
@@ -297,10 +461,8 @@ class Window(QWidget):
 
         for check in self.checkBoxes:
             check.setCheckState(Qt.Unchecked)
-        # self.checkBoxAll.setCheckState(Qt.PartiallyChecked)
-        # self.checkStates()
+
         text = self.lineEdit.text()
-        # print(type(self.information))
         count = 0
         for element in self.information:
             find_match = re.search(text, str(element).lower())
@@ -327,13 +489,11 @@ class Window(QWidget):
         self.checkBoxAll.blockSignals(True)
         self.checkBoxNone.blockSignals(True)
 
-        # set the "select all" fully checked too if all boxes are checked,
-        # otherwise make it partially checked
+        # set the "select all" fully checked too if all boxes are checked, otherwise make it partially checked
         self.checkBoxAll.setCheckState(
             Qt.Checked if all(states) else Qt.PartiallyChecked)
 
-        # set the "select none" unchecked only if all boxes are unchecked,
-        # otherwise make it partially checked
+        # set the "select none" unchecked only if all boxes are unchecked, otherwise make it partially checked
         self.checkBoxNone.setCheckState(
             Qt.Unchecked if not any(states) else Qt.PartiallyChecked)
 
@@ -342,11 +502,9 @@ class Window(QWidget):
         self.checkBoxNone.blockSignals(False)
 
     def selectBoxes(self, state):
-        # states = [c.isChecked() for c in self.checkBoxes]
 
         for check in self.checkBoxes:
 
-            # print(check)
             check.blockSignals(True)
             check.setChecked(state)
             check.blockSignals(False)
@@ -356,7 +514,8 @@ class Window(QWidget):
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
-    app.setApplicationName("No2Pads")
+    app.setApplicationName("Notepad")
 
     window = MainWindow()
     app.exec_()
+
